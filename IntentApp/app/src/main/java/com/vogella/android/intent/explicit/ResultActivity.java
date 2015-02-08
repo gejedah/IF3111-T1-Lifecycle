760008cdb3b1ffb05216612f37adf74aee7efff1
Kevin <kevin.zhong94@gmail.com>
2015-02-08 11:23:51 +0700

    Project kedua

---
+++ IntentApp/app/src/main/java/com/vogella/android/intent/explicit/ResultActivity.java
@@ -0,0 +1,17 @@
+package com.vogella.android.intent.explicit;
+
+/**
+ * Created by Kevin Zhong Local on 08/02/2015.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResultActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_result);
+    }
+}
